eaa7022b878af10957b5c8e8496e67b43775cb66
JeremyLapham <jeremylapham2004@gmail.com>
2023-10-05 20:39:58 -0700

    mobile nav

--- src/app/Components/mobileNavbar.tsx
+++ src/app/Components/mobileNavbar.tsx
@@ -5,18 +5,13 @@ export default function MobileNavbar() {
   return (
     <Navbar fixed='top' collapseOnSelect expand="xl" className="bg-body-tertiary d-sm-block d-md-block d-lg-block d-xl-none" >
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#introduction">Intro</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact Me</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
